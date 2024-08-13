dc88da6c747c3d4ea34d56d3b5fe7f4391fb3911
Yago Santos <yago.profissional.dev@outlook.com>
2024-08-13 13:09:25 -0300

    feat: Foram adicionadas novas tabelas no banco de dados e criadas models.

---
+++ app/Models/Boletim.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Boletim extends Model
+{
+    use HasFactory;
+
+    protected $table = 'boletins';
+
+    protected $fillable = [
+        'id',
+        'secao_id',
+        'aptos',
+        'assinatura_digital'
+    ];
+
+    public function secao() 
+    {
+        return $this->hasOne(Secao::class);
+    }
+
+}
