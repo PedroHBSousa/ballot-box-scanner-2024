e830c8683e0d7d772777f822956048ac3d1271ad
Pedro Henrique <pedro795027@gmail.com>
2024-08-21 16:08:57 -0300

    feat: correção de validações do controller "store"

--- app/Http/Controllers/ScannerController.php
+++ app/Http/Controllers/ScannerController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Boletim;
 use Illuminate\Support\Facades\Validator;
 use App\Services\BoletimEleitoralService;
+use Illuminate\Database\QueryException;
+use App\Models\Secao;
 
 class ScannerController extends Controller
 {
@@ -23,76 +25,80 @@ class ScannerController extends Controller
 
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'qrcode_value' => 'required',
-        ]);
+              // Validação inicial do input
+              $validator = Validator::make($request->all(), [
+                'qrcode_value' => 'required',
+            ]);
 
-        if ($validator->fails()) {
-            $request->session()->flash('error', 'O QRCode não foi escaneado');
-            return to_route('qrcodescanner');
-        }
-
-        $qrCodeValue = $request->input('qrcode_value');
-
-        $qrCodeValue = explode(" ", $qrCodeValue);
-
-        $qrCodeValue = array_map(function ($qrCodeValue) {
-            return explode(":", $qrCodeValue);
-        }, $qrCodeValue);
-
-        try {
-
-            // Verificar se MUNI e ZONA correspondem aos valores desejados
-            // foreach ($qrCodeValue as $pair) {
-            //     [$key, $value] = $pair;
-            //     if ($key === 'MUNI' && $value !== '71153') {
-            //         throw new \Exception('O QRCode não pertence ao município desejado.');
-            //     }
-            //     if ($key === 'ZONA' && $value !== '132') {
-            //         throw new \Exception('O QRCode não pertence à zona desejada.');
-            //     }
-            // }
-
-            // Recuperar QR codes lidos da sessão
-            $qrCodesLidos = $request->session()->get('qrCodesLidos', []);
-
-            $qrCodeService = new BoletimEleitoralService($qrCodeValue, $qrCodesLidos);
-            $status = $qrCodeService->getStatus();
-
-            // Atualizar os QR codes lidos na sessão
-            $request->session()->put('qrCodesLidos', $qrCodeService->getQrCodesLidos());
-
-            if ($status['qr_codes_restantes'] == 0) {
-                // Todos os QR codes foram lidos, prossiga para a verificação/armazenamento
-                $dadosBoletim = $qrCodeService->getDadosBoletim();
-                $boletimExistente = Boletim::where('ASSI', $dadosBoletim['ASSI'])->first();
-
-                if ($boletimExistente) {
-                    $request->session()->flash('error', 'Os QR codes lidos são referentes a um boletim existente no sistema.');
-                } else {
-                    // Criar novo boletim
-                    $dadosBoletim = [
-                        'secao_id' =>  $dadosBoletim['SECA'], // Certifique-se de passar o valor correto
-                        'apto' => $dadosBoletim['APTO'],
-                        'comp' => $dadosBoletim['COMP'],
-                        'falt' => $dadosBoletim['FALT'],
-                        'assinatura_digital' => $dadosBoletim['ASSI'],
-                    ];
-                    Boletim::create($dadosBoletim);
-                    $request->session()->flash('success', 'Boletim criado com sucesso.');
-                }
-
-                // Limpar sessão após o processo
-                $request->session()->forget('qrCodesLidos');
-            } else {
-                $request->session()->flash('status', "Você leu {$status['qr_codes_lidos']} de {$status['qr_codes_totais']} QR Codes. Faltam {$status['qr_codes_restantes']}.");
+            if ($validator->fails()) {
+                $request->session()->flash('error', 'O QRCode não foi escaneado.');
+                return redirect()->route('qrcodescanner');
             }
-        } catch (\Exception $e) {
-            $request->session()->flash('error', $e->getMessage());
-        }
 
-        return redirect()->route('qrcodescanner');
-        // dd($request->all());
+            // Processamento do valor do QRCode
+            $qrCodeValue = $request->input('qrcode_value');
+            $qrCodeArray = explode(" ", $qrCodeValue);
+            $qrCodeData = array_map(function ($item) {
+                return explode(":", $item);
+            }, $qrCodeArray);
+
+            try {
+                // Recuperar QR codes lidos da sessão
+                $qrCodesLidos = $request->session()->get('qrCodesLidos', []);
+
+                // Instanciar o serviço de Boletim Eleitoral
+                $qrCodeService = new BoletimEleitoralService($qrCodeData, $qrCodesLidos);
+                $status = $qrCodeService->getStatus();
+
+                // Atualizar os QR codes lidos na sessão
+                $request->session()->put('qrCodesLidos', $qrCodeService->getQrCodesLidos());
+
+                if ($status['qr_codes_restantes'] == 0) {
+                    // Todos os QR codes foram lidos, prossiga para a verificação/armazenamento
+                    $dadosBoletim = $qrCodeService->getDadosBoletim();
+
+                    // Verificar se o boletim já existe
+                    $boletimExistente = Boletim::where('assinatura_digital', $dadosBoletim['ASSI'])->first();
+
+                    if ($boletimExistente) {
+                        $request->session()->flash('error', 'Os QR codes lidos são referentes a um boletim existente no sistema.');
+                    } else {
+                        // Verificar se a seção existe
+                        $secaoExistente = Secao::find($dadosBoletim['SECA']);
+
+                        if (!$secaoExistente) {
+                            $request->session()->flash('error', 'A seção escaneada não existe no sistema.');
+                        } else {
+                            // Criar novo boletim
+                            Boletim::create([
+                                'secao_id' => $dadosBoletim['SECA'],
+                                'apto' => $dadosBoletim['APTO'],
+                                'comp' => $dadosBoletim['COMP'],
+                                'falt' => $dadosBoletim['FALT'],
+                                'assinatura_digital' => $dadosBoletim['ASSI'],
+                            ]);
+
+                            $request->session()->flash('success', 'Boletim criado com sucesso.');
+                        }
+                    }
+
+                    // Limpar sessão após o processo
+                    $request->session()->forget('qrCodesLidos');
+                } else {
+                    $request->session()->flash('status', "Você leu {$status['qr_codes_lidos']} de {$status['qr_codes_totais']} QR Codes. Faltam {$status['qr_codes_restantes']}.");
+                }
+            } catch (QueryException $e) {
+                // Captura erros relacionados ao banco de dados
+                if ($e->getCode() == "23000") { // Código de violação de integridade
+                    $request->session()->flash('error', 'Erro ao salvar o boletim: dados inválidos ou inconsistentes.');
+                } else {
+                    $request->session()->flash('error', 'Ocorreu um erro ao processar sua solicitação. Tente novamente mais tarde.');
+                }
+            } catch (\Exception $e) {
+                // Captura quaisquer outros erros
+                $request->session()->flash('error', $e->getMessage());
+            }
+            return redirect()->route('qrcodescanner');
     }
     public function clearQRCodes(Request $request)
     {
