737cde7bbaa759aee268ce226d93ca7d0aa745e6
Eduardo Lima <112963041+Eduardolima2003@users.noreply.github.com>
2024-08-22 14:04:32 -0300

    feat: foi colocado o filtro para os gráficos, porém ainda não foram colocados dados

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/qrcodescanner', [ScannerController::class, 'qrcodescanner'])->name(
 Route::get('/dashboard', [ScannerController::class, 'dashboard']);
 Route::post('/store', [ScannerController::class, 'store'])->name('store');
 Route::post('/qrcodescanner/clear', [ScannerController::class, 'clearQRCodes'])->name('qrcodes.clear');
-Route::get('/get-chart-data/{filter}', [ChartController::class, 'getChartData']);
+Route::get ('/api/chart-data/{filter}', [ChartController::class , 'getChartData']);
