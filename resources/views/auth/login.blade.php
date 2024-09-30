05db19fd2726118b089be8b3099a8db3ec05d88d
Pedro Henrique <pedro795027@gmail.com>
2024-09-30 18:07:44 -0300

    feat: estilização do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login</title>
     @vite('resources/css/login.css')
 </head>
+
 <body>
     <!-- resources/views/auth/login.blade.php -->
-<form action="{{ route('login') }}" method="POST">
-    @csrf
-    <div>
-        <label for="password">Chave de Acesso</label>
-        <input type="password" name="password" id="password">
+    <div class="container-image">
+        <img id="felipe" src="{{ Vite::asset('resources/img/Felipe.png') }}">
+        <img id="reis" src="{{ Vite::asset('resources/img/Reis.png') }}">
+        <img id="reinaldinho" src="{{ Vite::asset('resources/img/Reinaldinho.png') }}">
     </div>
-    @error('password')
-        <div class="error-message">{{ $message }}</div>
-    @enderror
-    <button type="submit">Entrar</button>
-</form>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <div>
+            <label for="password">Chave de Acesso</label>
+            <input type="password" name="password" id="password">
+        </div>
+        @error('password')
+            <div class="error-message">{{ $message }}</div>
+        @enderror
+        <button type="submit">Entrar</button>
+    </form>
 
 </body>
+
 </html>
