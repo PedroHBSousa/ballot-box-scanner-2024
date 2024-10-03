2c523b308b94c8c0e5a78c63a820d73a3a7868e0
Pedro Henrique <pedro795027@gmail.com>
2024-10-03 16:19:07 -0300

    feat: correção do tempo de sessão

--- config/session.php
+++ config/session.php
@@ -32,9 +32,9 @@ return [
     |
     */
 
-    'lifetime' => 1,
+    'lifetime' => 120,
 
-    'expire_on_close' => true,
+    'expire_on_close' => false,
 
     /*
     |--------------------------------------------------------------------------
