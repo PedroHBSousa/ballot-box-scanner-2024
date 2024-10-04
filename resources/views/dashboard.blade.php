935c2f0bdd10f396611711b6dff2f26da265c065
Pedro Henrique <pedro795027@gmail.com>
2024-10-04 15:41:58 -0300

    feat: alterações de nomes dos arquivos js

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,8 +13,8 @@
         href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap"
         rel="stylesheet">
     @vite('resources/css/dashboard.css')
-    <script src="{{ asset('js/charts.js') }}"></script>
-    <script src="{{ asset('js/html2canvas.min.js') }}"></script>
+    <script src="{{ asset('js/chartss.js') }}"></script>
+    <script src="{{ asset('js/html2canvas.minn.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
